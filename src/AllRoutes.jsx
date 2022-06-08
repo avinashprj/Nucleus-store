d63c098e420555207cf0746875928be53174bbb9
avipj <avinashprajapati914@gmail.com>
2022-06-08 23:06:37 +0530

    Feat: Added new Routes

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage/HomePage';
+import {
+  HomePage,
+  ErrorPage,
+  Wishlist,
+  AboutPage,
+  ProductListing,
+} from './pages/index';
 
 const AllRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="*" element={<HomePage />} />
+    <Route path="/wishlist" element={<Wishlist />} />
+    <Route path="/about" element={<AboutPage />} />
+    <Route path="/products" element={<ProductListing />} />
+    <Route
+      path="/products/:productID"
+      element={<div style={{ fontSize: '10rem' }}>SINGLE PRODUCT</div>}
+    />
+    <Route path="*" element={<ErrorPage />} />
   </Routes>
 );
 
