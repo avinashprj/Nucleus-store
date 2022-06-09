2ca8f31d72067aa0586eafd2b6e30dcc0b91f0e0
avipj <avinashprajapati914@gmail.com>
2022-06-09 23:58:01 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,28 @@
-import { HomePage } from './HomePage/HomePage';
+
 import { ErrorPage } from './ErrorPage/ErrorPage';
 import { Wishlist } from './Wishlist/Wishlist';
 import { AboutPage } from './AboutPage/AboutPage';
 import { ProductListing } from './ProductListingPage/ProductListing';
+import { LoginPage } from './LoginPage/LoginPage';
+import { SignupPage } from './SignupPage/SignupPage';
+import { SingleProduct } from './SingleProduct/SingleProduct';
 
-export { HomePage, ErrorPage, Wishlist, AboutPage, ProductListing };
+
+
+export {
+  HomePage,
+  ErrorPage,
+  Wishlist,
+  AboutPage,
+  ProductListing,
+  LoginPage,
+  SignupPage,
+  SingleProduct
+};
+
+
+
+
+
+
+
