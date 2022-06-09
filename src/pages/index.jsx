a933414d511a8dcfd903c7ef85951f2da1a63caa
avipj <avinashprajapati914@gmail.com>
2022-06-09 14:05:28 +0530

    Feat: Added phone filter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,17 @@
 import { HomePage } from './HomePage/HomePage';
+import { ErrorPage } from './ErrorPage/ErrorPage';
+import { Wishlist } from './Wishlist/Wishlist';
+import { AboutPage } from './AboutPage/AboutPage';
+import { ProductListing } from './ProductListingPage/ProductListing';
+import { LoginPage } from './LoginPage/LoginPage';
+import { SignupPage } from './SignupPage/SignupPage';
 
-export { HomePage };
+export {
+  HomePage,
+  ErrorPage,
+  Wishlist,
+  AboutPage,
+  ProductListing,
+  LoginPage,
+  SignupPage,
+};
