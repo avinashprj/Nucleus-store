868a26030d75ce21abc33ad9aeda0c0013c1ade6
avipj <avinashprajapati914@gmail.com>
2022-06-10 00:16:24 +0530

    Update ProductListing.jsx

--- src/pages/ProductListingPage/ProductListing.jsx
+++ src/pages/ProductListingPage/ProductListing.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+import { Footer, Navbar, FiltersDesktop } from '../../components';
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+import { Footer, Navbar, FiltersDesktop } from '../../components';
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 import {
   Footer,
   Navbar,
@@ -6,21 +18,62 @@ import {
   FilterPhone,
   ProductCard,
 } from '../../components';
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 import { products } from '../../backend/db/products';
 
 export const ProductListing = () => {
   const [state, useState] = React.useState();
   return (
     <>
+<<<<<<< Updated upstream
       <header>
         <Navbar />
       </header>
+=======
+<<<<<<< Updated upstream
+      <header>
+        <Navbar />
+      </header>
+=======
+<<<<<<< Updated upstream
+      <header>
+        <Navbar />
+      </header>
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       <main>
         <section className="page-hero">
           <div className="section-center">
             <h3 className="page-hero-title">
+<<<<<<< Updated upstream
               Home <span className="title-slash">/</span> Products (
               <span className="spc-txt">{products.length}</span>)
+=======
+<<<<<<< Updated upstream
+              Home <span className="title-slash">/</span> Products
+=======
+<<<<<<< Updated upstream
+              Home <span className="title-slash">/</span> Products (
+              <span className="spc-txt">{products.length}</span>)
+=======
+<<<<<<< Updated upstream
+              Home <span className="title-slash">/</span> Products
+=======
+              Home <span className="title-slash">/</span> Products (
+              <span className="spc-txt">{products.length}</span>)
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             </h3>
           </div>
         </section>
@@ -28,6 +81,22 @@ export const ProductListing = () => {
           <section className="sticky filter-container">
             <FiltersDesktop />
           </section>
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+        </section>
+      </main>
+      <Footer />
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+        </section>
+      </main>
+      <Footer />
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
           <section className="products-container">
             {products.map((product) => (
               <ProductCard key={product.id} singleProduct={product} />
@@ -36,7 +105,15 @@ export const ProductListing = () => {
         </section>
       </main>
       <FilterPhone />
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
       <Footer />
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     </>
   );
 };
