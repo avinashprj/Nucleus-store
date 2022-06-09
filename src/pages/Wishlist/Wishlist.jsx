c45386778ff186df49ef133824c1c735719c98d0
avipj <avinashprajapati914@gmail.com>
2022-06-09 23:40:17 +0530

    made changes to the cart And navbar

---
+++ src/pages/Wishlist/Wishlist.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BsFillHeartFill, BsStarFill } from 'react-icons/bs';
+import { Navbar, Footer, ProductCard } from '../../components';
+import { products } from '../../backend/db/products';
+
+export const Wishlist = () => (
+  <main>
+    <section className="page-hero">
+      <div className="section-center">
+        <h3 className="page-hero-title">
+          <span className="title-slash">/</span> Wishlist
+        </h3>
+      </div>
+    </section>
+    <section className="wishlist-products">
+      <div
+        className={`products ${products?.length !== 1 ? 'more-products' : ''}`}
+      >
+        {products.map((product) => (
+          <ProductCard key={product.id} singleProduct={product}>
+            Go to Cart
+          </ProductCard>
+        ))}
+      </div>
+    </section>
+  </main>
+);
