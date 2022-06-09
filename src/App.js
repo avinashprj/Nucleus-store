f8257ad01b3242f16e83fb6d31f6addd9fa9ea43
avipj <avinashprajapati914@gmail.com>
2022-06-10 02:21:08 +0530

    Update App.js
    Added scroll to top for routing between different sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import AllRoutes from './AllRoutes';
+import { useScrollToTop } from './utils/utils';
 
 function App() {
+  useScrollToTop();
   return (
     <div className="App">
       <AllRoutes />
