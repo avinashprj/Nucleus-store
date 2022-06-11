ccdf9fb9727acacd06c68351660d391589291e60
avipj <avinashprajapati914@gmail.com>
2022-06-12 03:14:00 +0530

    Added LocalStorage using react-query

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental';
+import { createWebStoragePersistor } from 'react-query/createWebStoragePersistor-experimental';
 import App from './App';
 import { makeServer } from './server';
 import { ErrorFallback } from './utils/utils';
 import { ProductContextProvider } from './store/index.store';
 // Call make Server
 makeServer();
-const queryClient = new QueryClient(); // note: make instance of QueryClient
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+}); // note: make instance of QueryClient
+
+const localStoragePersistor = createWebStoragePersistor({
+  storage: window.localStorage,
+});
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
+});
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
