6db05c796763771f875171121a0e1392c8eb8084
avipj <avinashprajapati914@gmail.com>
2022-06-17 02:22:38 +0530

    feat: added cart functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
 import { FiX } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { useCloseOnClickOutside } from '../../CustomHooks/CustomHooks';
-
-import { products } from '../../backend/db/products';
+import {
+  useCartContext,
+  useDeleteFromCart,
+  useUpdateCart,
+} from '../../store/Context/CartContext';
+import { useAuthContext } from '../../store/Context/AuthContext';
+import {
+  useAddToWishlist,
+  useWishlistContext,
+} from '../../store/Context/WishlistContext';
+import {
+  cartTotalPrice,
+  formatPrice,
+  isPresentInState,
+} from '../../utils/utils';
+import { usePaymentIntegration } from '../../CustomHooks/usePaymentIntegration';
 
 export const Cart = ({ toggleCartModal, setToggleCartModal }) => {
+  const { user } = useAuthContext();
   const cartRef = React.useRef(null);
+  const { setCart, cart } = useCartContext();
+  const [wishlist, setWishlist] = useWishlistContext();
+
+  const { mutate: removeFromCart, isLoading: isRemovingCart } =
+    useDeleteFromCart();
+  const { mutate: addToWishlist } = useAddToWishlist();
+
+  const { mutate: changeQuantityCart } = useUpdateCart();
+
+  const { displayRazorpay } = usePaymentIntegration();
   useCloseOnClickOutside(cartRef, setToggleCartModal);
+
   return (
     <aside className={`cart-overlay ${toggleCartModal ? 'show' : ''}`}>
       <aside ref={cartRef} className="cart">
@@ -18,116 +46,168 @@ export const Cart = ({ toggleCartModal, setToggleCartModal }) => {
         >
           <FiX />
         </button>
-
-        <h3 className="text-slanted">your bag</h3>
-
-        {/* cart items */}
-        <div className="cart-items">
-          <article className="cart-item" data-id="rec43w3ipXvP28vog">
-            <img
-              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Mercury-1_540x.png?v=1639400857"
-              className="cart-item-img"
-              alt="high-back bench"
-            />
-            <div>
-              <h4 className="cart-item-name">Smart Watch</h4>
-              <p className="cart-item-price">₹749.25</p>
-              <button
-                type="submit"
-                className="cart-item-remove-btn"
-                data-id="rec43w3ipXvP28vog"
-              >
-                Remove
-              </button>
-              <button
-                type="submit"
-                className="m-left-small cart-item-remove-btn"
-                data-id="rec43w3ipXvP28vog"
-              >
-                Wishlist
-              </button>
-            </div>
-            <div className="">
-              <button
-                type="submit"
-                className="cart-item-increase-btn "
-                data-id="rec43w3ipXvP28vog"
-              >
-                <IoIosArrowUp className="link fs-medium" />
-              </button>
-              <p className="cart-item-amount" data-id="rec43w3ipXvP28vog">
-                2
+        <h3 className="text-slanted center">Your Bag</h3>
+        {user.id ? (
+          <div className="cart-items">
+            {cart?.length > 0 &&
+              cart?.map((product) => (
+                <article
+                  key={product._id}
+                  className="cart-item"
+                  data-id={product._id}
+                >
+                  <img
+                    src={product.imgUrl}
+                    className="cart-item-img"
+                    alt="high-back bench"
+                  />
+                  <div>
+                    <h4 className="cart-item-name">{product.productTitle}</h4>
+                    <p className="cart-item-price">₹{product.productPrice}</p>
+                    <button
+                      onClick={() =>
+                        removeFromCart(product._id, {
+                          onSuccess: ({ data: { cart: cartData } }) => {
+                            toast.success('removed from cart');
+                            setCart(cartData);
+                          },
+                          onError: (error) => {
+                            toast.error(error.message);
+                          },
+                        })
+                      }
+                      type="submit"
+                      className="cart-item-remove-btn"
+                      data-id={product.id}
+                    >
+                      Remove
+                    </button>
+                    <button
+                      onClick={() => {
+                        removeFromCart(product._id, {
+                          onSuccess: ({ data: { cart: cartData } }) => {
+                            setCart(cartData);
+                          },
+                          onError: (error) => {
+                            toast.error(error.message);
+                          },
+                        });
+                        if (!isPresentInState(product, wishlist)) {
+                          addToWishlist(product, {
+                            onSuccess: ({
+                              data: { wishlist: wishListData },
+                            }) => {
+                              toast.success('added to wishlist');
+                              setWishlist(wishListData);
+                            },
+                            onError: (error) => {
+                              toast.error(error.message);
+                            },
+                          });
+                        } else {
+                          toast.info('Already in Wishlist ');
+                        }
+                      }}
+                      type="submit"
+                      className="m-left-small cart-item-remove-btn"
+                      data-id={product.id}
+                    >
+                      Wishlist
+                    </button>
+                  </div>
+                  <div className="">
+                    <button
+                      onClick={() => {
+                        changeQuantityCart(
+                          { product, type: 'increment' },
+                          {
+                            onSuccess: ({ data: { cart: cardData } }) => {
+                              setCart(cardData);
+                            },
+                            onError: (error) => {
+                              toast.error(error.message);
+                            },
+                          }
+                        );
+                      }}
+                      type="button"
+                      className="cart-item-increase-btn "
+                      data-id={product.id}
+                    >
+                      <IoIosArrowUp className="link fs-medium" />
+                    </button>
+                    <p className="cart-item-amount" data-id={product.id}>
+                      {product.qty}
+                    </p>
+                    <button
+                      onClick={() => {
+                        if (product.qty >= 2) {
+                          changeQuantityCart(
+                            { product, type: 'decrement' },
+                            {
+                              onSuccess: ({ data: { cart: cardData } }) => {
+                                setCart(cardData);
+                              },
+                              onError: (error) => {
+                                toast.error(error.message);
+                              },
+                            }
+                          );
+                        } else {
+                          removeFromCart(product._id, {
+                            onSuccess: ({ data: { cart: cartData } }) => {
+                              setCart(cartData);
+                            },
+                            onError: (error) => {
+                              toast.error(error.message);
+                            },
+                          });
+                        }
+                      }}
+                      type="button"
+                      className="cart-item-decrease-btn"
+                      data-id="rec43w3ipXvP28vog"
+                    >
+                      <IoIosArrowDown className="link fs-medium" />
+                    </button>
+                  </div>
+                </article>
+              ))}
+            {cart?.length === 0 && (
+              <p className="center" style={{ marginTop: '4rem' }}>
+                Add some products to the cart
               </p>
+            )}
+          </div>
+        ) : (
+          <div style={{ display: 'grid', placeItems: 'center' }}>
+            <Link to="/login">
               <button
-                type="submit"
-                className="cart-item-decrease-btn"
-                data-id="rec43w3ipXvP28vog"
+                className="btn btn-squared"
+                type="button"
+                onClick={() => setToggleCartModal((prev) => !prev)}
               >
-                <IoIosArrowDown className="link fs-medium" />
+                <h3>PLEASE LOG IN FIRST</h3>
               </button>
-            </div>
-          </article>
-        </div>
-
-        <div className="cart-items">
-          {products.map((product) => (
-            <article
-              key={product.id}
-              className="cart-item"
-              data-id={product.id}
+            </Link>
+          </div>
+        )}
+        {user.id ? (
+          <footer>
+            <h3 className="cart-total text-slanted">
+              total: {cart && formatPrice(cartTotalPrice(cart))}
+            </h3>
+            <button
+              onClick={() => {
+                displayRazorpay();
+              }}
+              type="submit"
+              className="cart-checkout btn btn-squared "
             >
-              <img
-                src={product.imgUrl}
-                className="cart-item-img"
-                alt="high-back bench"
-              />
-              <div>
-                <h4 className="cart-item-name">{product.productTitle}</h4>
-                <p className="cart-item-price">₹{product.productDiscpercent}</p>
-                <button
-                  type="submit"
-                  className="cart-item-remove-btn"
-                  data-id={product.id}
-                >
-                  Remove
-                </button>
-                <button
-                  type="submit"
-                  className="m-left-small cart-item-remove-btn"
-                  data-id={product.id}
-                >
-                  Wishlist
-                </button>
-              </div>
-              <div className="">
-                <button
-                  type="submit"
-                  className="cart-item-increase-btn "
-                  data-id={product.id}
-                >
-                  <IoIosArrowUp className="link fs-medium" />
-                </button>
-                <p className="cart-item-amount" data-id={product.id}>
-                  2
-                </p>
-                <button
-                  type="submit"
-                  className="cart-item-decrease-btn"
-                  data-id="rec43w3ipXvP28vog"
-                >
-                  <IoIosArrowDown className="link fs-medium" />
-                </button>
-              </div>
-            </article>
-          ))}
-        </div>
-        {/* footer */}
-        <footer>
-          <h3 className="cart-total text-slanted">total : $12.99</h3>
-          <button type="submit" className="cart-checkout btn btn-squared ">
-            checkout
-          </button>
-        </footer>
+              checkout
+            </button>
+          </footer>
+        ) : null}
       </aside>
     </aside>
   );
