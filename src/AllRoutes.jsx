4fa57981e65f7a762fa918ec4c5d23367d129bc5
avipj <avinashprajapati914@gmail.com>
2022-06-17 02:04:06 +0530

    feat: added private route

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import { Navbar, Footer, SharedLayout } from './components';
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
 
 import {
   HomePage,
@@ -17,9 +18,12 @@ const AllRoutes = () => (
   <Routes>
     <Route path="/" element={<SharedLayout />}>
       <Route index element={<HomePage />} />
-      <Route path="/wishlist" element={<Wishlist />} />
+
       <Route path="/about" element={<AboutPage />} />
       <Route path="/products" element={<ProductListing />} />
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/products/:productID" element={<SingleProduct />} />
