419d3231b7517ca0ec0033ee9e8753b78e4e8619
avipj <avinashprajapati914@gmail.com>
2022-06-17 02:22:13 +0530

    added all the functions of cart

--- src/store/Context/CartContext.jsx
+++ src/store/Context/CartContext.jsx
@@ -1,2 +1,48 @@
 import React from 'react';
 import axios from 'axios';
+import { useMutation } from 'react-query';
+import {
+  AddToCart,
+  DeleteFromCart,
+  UpdateCart,
+} from '../../serverCalls/index.server';
+
+const CartContext = React.createContext();
+
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = React.useState([]);
+  const [toggleCartModal, setToggleCartModal] = React.useState(false);
+
+  const value = React.useMemo(
+    () => ({
+      cart,
+      setCart,
+      toggleCartModal,
+      setToggleCartModal,
+    }),
+    [cart, toggleCartModal]
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+const useCartContext = () => {
+  const context = React.useContext(CartContext);
+  if (context === 'undefined') {
+    throw new Error(
+      'useCartContext must be use in a CartContextProvider component'
+    );
+  }
+  return context;
+};
+
+const useAddToCart = () => useMutation(AddToCart);
+const useUpdateCart = () => useMutation(UpdateCart);
+const useDeleteFromCart = () => useMutation(DeleteFromCart);
+export {
+  useCartContext,
+  CartContextProvider,
+  useAddToCart,
+  useUpdateCart,
+  useDeleteFromCart,
+};
