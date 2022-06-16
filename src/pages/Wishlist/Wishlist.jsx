12907bf9c8e5a63418b0b6beeb86f462492e6fa5
avipj <avinashprajapati914@gmail.com>
2022-06-17 02:28:16 +0530

    added fallback to the wishlist

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { ProductCard } from '../../components';
 import { products } from '../../backend/db/products';
+import { useWishlistContext } from '../../store/Context/WishlistContext';
+import { NoProductsFound } from '../../components/NoProductsFound/NoProductsFound';
 
-export const Wishlist = () => (
-  <main>
-    <section className="page-hero">
-      <div className="section-center">
-        <h3 className="page-hero-title">
-          <span className="title-slash">/</span> Wishlist
-        </h3>
-      </div>
-    </section>
-    <section className="wishlist-products">
-      <div
-        className={`products ${products?.length !== 1 ? 'more-products' : ''}`}
-      >
-        {products.map((product) => (
-          <ProductCard key={product.id} singleProduct={product}>
-            Go to Cart
-          </ProductCard>
-        ))}
-      </div>
-    </section>
-  </main>
-);
+export const Wishlist = () => {
+  const [wishlist] = useWishlistContext();
+
+  return (
+    <main>
+      <section className="page-hero">
+        <div className="section-center">
+          <h3 className="page-hero-title">
+            <span className="title-slash">/</span> Wishlist
+          </h3>
+        </div>
+      </section>
+      <section className="wishlist-products">
+        <div
+          className={`products ${wishlist?.length >= 1 ? 'more-products' : ''}`}
+        >
+          {wishlist?.length === 0 && <NoProductsFound />}
+          {wishlist?.map((product) => (
+            <ProductCard key={product._id} singleProduct={product} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
