21a8541b2a98d3e573ee8cdb901e30a5d5556f6d
avipj <avinashprajapati914@gmail.com>
2022-06-17 02:04:20 +0530

    added toast to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
 import AllRoutes from './AllRoutes';
 import { useScrollToTop } from './CustomHooks/CustomHooks';
 
@@ -7,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <AllRoutes />
+      <ToastContainer autoClose={1500} />
     </div>
   );
 }
