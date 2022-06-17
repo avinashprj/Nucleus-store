9f9efdd137d2e910f5a363a0d7b823642a121d7b
avipj <avinashprajapati914@gmail.com>
2022-06-18 01:37:18 +0530

    added temp loader

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import './App.css';
 import { ToastContainer } from 'react-toastify';
 import AllRoutes from './AllRoutes';
 import { useScrollToTop } from './CustomHooks/CustomHooks';
+import { Loader } from './components/Loader/Loader';
 
 function App() {
   useScrollToTop();
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
-      <AllRoutes />
-      <ToastContainer autoClose={1500} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <AllRoutes />
+          <ToastContainer autoClose={1500} />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Loader = () => (
+  <div className="loader-outer">
+    <div className="main">
+      <div className="loader-inner">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  </div>
+);
