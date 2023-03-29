036c546da083e2f6210b71eddcb49f82676ece38
avinashprj <avinashprajapati914@gmail.com>
2023-03-29 10:27:18 +0530

    fix: disable-eslint for service workers

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-
+/* eslint-disable */
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
 // for the list of available Workbox modules, or add any other
@@ -43,7 +43,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(`${process.env.PUBLIC_URL}/index.html`)
 );
 
 // An example runtime caching route for requests that aren't handled by the
